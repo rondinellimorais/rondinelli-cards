35c06759a142864de5b504b0de44d596d249a471
RONDINELE SANTOS DE MORAIS <60003107@rondinellimorais.riachuelo.net>
2020-02-11 17:32:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+const Cards = () => (
+  <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text style={{ color: 'red', fontSize: 18, fontWeight: 'bold' }}>Home</Text>
+  </SafeAreaView>
+);
+
+export default Cards;
